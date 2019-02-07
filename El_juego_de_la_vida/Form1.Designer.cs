1bac0b86218d89acae2fc16bfa3faae46b44f46a
Jafet <alexis-jafet.020196@hotmail.com>
2019-02-07 17:47:30 -0600

    1er Respaldo El juego de la vida

---
+++ El_juego_de_la_vida/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace El_juego_de_la_vida
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button_ngen = new System.Windows.Forms.Button();
+            this.buttonreinicio = new System.Windows.Forms.Button();
+            this.ButtonSucesivas = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.salir = new System.Windows.Forms.Button();
+            this.buttonpase = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Arial Unicode MS", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(742, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(162, 112);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Seleccione el estado inicial";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // button_ngen
+            // 
+            this.button_ngen.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button_ngen.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button_ngen.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button_ngen.Location = new System.Drawing.Point(774, 177);
+            this.button_ngen.Name = "button_ngen";
+            this.button_ngen.Size = new System.Drawing.Size(102, 41);
+            this.button_ngen.TabIndex = 2;
+            this.button_ngen.Text = "Nueva generacion";
+            this.button_ngen.UseVisualStyleBackColor = false;
+            this.button_ngen.Click += new System.EventHandler(this.button_ngen_Click);
+            // 
+            // buttonreinicio
+            // 
+            this.buttonreinicio.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.buttonreinicio.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonreinicio.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonreinicio.Location = new System.Drawing.Point(774, 407);
+            this.buttonreinicio.Name = "buttonreinicio";
+            this.buttonreinicio.Size = new System.Drawing.Size(102, 40);
+            this.buttonreinicio.TabIndex = 3;
+            this.buttonreinicio.Text = "Reiniciar";
+            this.buttonreinicio.UseVisualStyleBackColor = false;
+            this.buttonreinicio.Click += new System.EventHandler(this.buttonreinicio_Click);
+            // 
+            // ButtonSucesivas
+            // 
+            this.ButtonSucesivas.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ButtonSucesivas.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ButtonSucesivas.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.ButtonSucesivas.Location = new System.Drawing.Point(774, 259);
+            this.ButtonSucesivas.Name = "ButtonSucesivas";
+            this.ButtonSucesivas.Size = new System.Drawing.Size(102, 42);
+            this.ButtonSucesivas.TabIndex = 4;
+            this.ButtonSucesivas.Text = "Generaciones sucesivas";
+            this.ButtonSucesivas.UseVisualStyleBackColor = false;
+            this.ButtonSucesivas.Click += new System.EventHandler(this.ButtonSucesivas_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // salir
+            // 
+            this.salir.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.salir.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.salir.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.salir.Location = new System.Drawing.Point(802, 573);
+            this.salir.Name = "salir";
+            this.salir.Size = new System.Drawing.Size(102, 42);
+            this.salir.TabIndex = 5;
+            this.salir.Text = "Salir";
+            this.salir.UseVisualStyleBackColor = false;
+            this.salir.Click += new System.EventHandler(this.salir_Click);
+            // 
+            // buttonpase
+            // 
+            this.buttonpase.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.buttonpase.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonpase.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonpase.Location = new System.Drawing.Point(790, 307);
+            this.buttonpase.Name = "buttonpase";
+            this.buttonpase.Size = new System.Drawing.Size(75, 32);
+            this.buttonpase.TabIndex = 6;
+            this.buttonpase.Text = "Pausar";
+            this.buttonpase.UseVisualStyleBackColor = false;
+            this.buttonpase.Click += new System.EventHandler(this.buttonpase_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.ClientSize = new System.Drawing.Size(924, 677);
+            this.Controls.Add(this.buttonpase);
+            this.Controls.Add(this.salir);
+            this.Controls.Add(this.ButtonSucesivas);
+            this.Controls.Add(this.buttonreinicio);
+            this.Controls.Add(this.button_ngen);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "El juego de la vida";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button_ngen;
+        private System.Windows.Forms.Button buttonreinicio;
+        private System.Windows.Forms.Button ButtonSucesivas;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button salir;
+        private System.Windows.Forms.Button buttonpase;
+    }
+}
+
---
+++ El_juego_de_la_vida/Form1.cs
@@ -0,0 +1,490 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+
+namespace El_juego_de_la_vida
+{
+    public partial class Form1 : Form
+    {
+        const int T=16;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        PictureBox[,] matrix = new PictureBox[T, T];
+        int[,] vida = new int[T, T];
+        int[,] sig_gen = new int[T, T];
+        int[,] vecino = new int[T, T];
+
+
+        //Metodo para inicializar los picturesBox
+        public void inicializar_tablero()
+        {
+            int i, j;
+            for (i = 0; i <= 15; i++)
+            {
+                for(j = 0; j <= 15; j++)
+                {
+                    matrix[i, j] = new PictureBox();
+                    Controls.Add(matrix[i, j]);
+                    matrix[i, j].Width = 40;
+                    matrix[i, j].Height =40;
+                    matrix[i, j].Click += new EventHandler(this.Cambio_Click);
+                    if(j == 0)
+                        matrix[i, j].Left = 100;
+                    matrix[i, j].Top = 40 * i;
+                    matrix[i, j].Left = 100 + (40 * j);
+                }
+            }
+        }
+
+
+        //Establecer posiciones iniciales (imagenes)
+        private void Cambio_Click(object sender, EventArgs e)
+        {
+
+            string index;
+            PictureBox clickedPicture = (PictureBox)sender;
+            index = clickedPicture.BackColor.ToString();
+
+            switch(index)
+            {
+                case "Color [White]": clickedPicture.BackColor = Color.Blue;
+                    clickedPicture.Image = Image.FromFile("vivo.png");
+                    break;
+
+                case "Color [Blue]":  clickedPicture.BackColor = Color.Green;
+                    clickedPicture.Image = Image.FromFile("feliz.png");
+                    break;
+
+                case "Color [Green]":
+                    clickedPicture.BackColor = Color.White;
+                    clickedPicture.Image = Image.FromFile("muerto.png");
+                    break;
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            inicializar_tablero();
+            Vida_inicio();
+        }
+
+        //Estado inicial       (imagenes)
+        public void Vida_inicio()
+        {
+            int i, j;
+            for (i = 0; i <= 15; i++)
+            {
+                for (j = 0; j <= 15; j++)
+                {
+                    vida[i, j] = 0;
+                    matrix[i, j].BackColor = Color.White;
+                    matrix[i, j].Image = Image.FromFile("muerto.png");
+                }
+            }
+        }
+
+
+        public void Evaluar_vecinos()
+        {
+            int indicer, indicec;
+            int i, j;
+            for (i = 0; i <= 15; i++)
+            {
+                for (j = 0; j <= 15; j++)
+                {
+                    //4 reglas
+                    indicer = i;
+                    indicec = j;
+
+                    //8 vecinos a evaluar
+                    vecino[i, j] = 0;
+
+                    if (i == 0) 
+                    {
+                        if (j == 0) 
+                        {
+                            //5, 7, 8
+                            //5
+                            if (vida[indicer, indicec + 1] == 1 || vida[indicer, indicec + 1] == 2)
+                            {
+                                vecino[i, j]++;
+                            }
+                            //7
+                            if (vida[indicer + 1, indicec] == 1 || vida[indicer + 1, indicec] == 2)
+                            {
+                                vecino[i, j]++;
+                            }
+                            //8
+                            if (vida[indicer + 1, indicec + 1] == 1 || vida[indicer + 1, indicec + 1] == 2)
+                            {
+                                vecino[i, j]++;
+                            }
+                        }
+                        if (j == 15) 
+                        {
+                            //4, 6, 7
+                            //4
+                            if (vida[indicer, indicec - 1] == 1 || vida[indicer, indicec - 1] == 2)
+                            {
+                                vecino[i, j]++;
+                            }
+                            //6
+                            if (vida[indicer + 1, indicec - 1] == 1 || vida[indicer + 1, indicec - 1] == 2)
+                            {
+                                vecino[i, j]++;
+                            }
+                            //7
+                            if (vida[indicer + 1, indicec] == 1 || vida[indicer + 1, indicec] == 2)
+                            {
+                                vecino[i, j]++;
+                            }
+
+                        }
+                        if (j != 0 && j != 15) 
+                        {
+                            ////4, 5, 6, 7, 8
+                            //4
+                            if (vida[indicer, indicec - 1] == 1 || vida[indicer, indicec - 1] == 2)
+                            {
+                                vecino[i, j]++;
+                            }
+                            //5
+                            if (vida[indicer, indicec + 1] == 1 || vida[indicer, indicec + 1] == 2)
+                            {
+                                vecino[i, j]++;
+                            }
+                            //6
+                            if (vida[indicer + 1, indicec - 1] == 1 || vida[indicer + 1, indicec - 1] == 2)
+                            {
+                                vecino[i, j]++;
+                            }
+                            //7
+                            if (vida[indicer + 1, indicec] == 1 || vida[indicer + 1, indicec] == 2)
+                            {
+                                vecino[i, j]++;
+                            }
+                            //8
+                            if (vida[indicer + 1, indicec + 1] == 1 || vida[indicer + 1, indicec + 1] == 2)
+                            {
+                                vecino[i, j]++;
+                            }
+
+                        }
+                    }
+                    if (i == 15)
+                    {
+                        if (j == 0)
+                        {
+                            //2, 3, 5
+                            //2
+                            if (vida[indicer - 1, indicec] == 1 || vida[indicer - 1, indicec] == 2)
+                            {
+                                vecino[i, j]++;
+                            }
+                            //3
+                            if (vida[indicer - 1, indicec + 1] == 1 || vida[indicer - 1, indicec + 1] == 2)
+                            {
+                                vecino[i, j]++;
+                            }
+                            //5
+                            if (vida[indicer, indicec + 1] == 1 || vida[indicer, indicec + 1] == 2)
+                            {
+                                vecino[i, j]++;
+                            }
+                        }
+                        if (j == 15)
+                        {
+                            //1, 2, 4
+                            //1
+                            if (vida[indicer - 1, indicec - 1] == 1 || vida[indicer - 1, indicec - 1] == 2)
+                            {
+                                vecino[i, j]++;
+                            }
+                            //2
+                            if (vida[indicer - 1, indicec] == 1 || vida[indicer - 1, indicec] == 2)
+                            {
+                                vecino[i, j]++;
+                            }
+                            //4
+                            if (vida[indicer, indicec - 1] == 1 || vida[indicer, indicec - 1] == 2)
+                            {
+                                vecino[i, j]++;
+                            }
+                        }
+                        if (j != 0 && j != 15)
+                        {
+                            ////1, 2, 3, 4, 5
+                            //1
+                            if (vida[indicer - 1, indicec - 1] == 1 || vida[indicer - 1, indicec - 1] == 2)
+                            {
+                                vecino[i, j]++;
+                            }
+                            //2
+                            if (vida[indicer - 1, indicec] == 1 || vida[indicer - 1, indicec] == 2)
+                            {
+                                vecino[i, j]++;
+                            }
+                            //3
+                            if (vida[indicer - 1, indicec + 1] == 1 || vida[indicer - 1, indicec + 1] == 2)
+                            {
+                                vecino[i, j]++;
+                            }
+                            //4
+                            if (vida[indicer, indicec - 1] == 1 || vida[indicer, indicec - 1] == 2)
+                            {
+                                vecino[i, j]++;
+                            }
+                            //5
+                            if (vida[indicer, indicec + 1] == 1 || vida[indicer, indicec + 1] == 2)
+                            {
+                                vecino[i, j]++;
+                            }
+                        }
+                    }
+
+                    if (j == 0 && i != 0 && i != 15)
+                    {
+                        //2, 3, 5, 7, 8
+                        //2
+                        if (vida[indicer - 1, indicec] == 1 || vida[indicer - 1, indicec] == 2)
+                        {
+                            vecino[i, j]++;
+                        }
+                        //3
+                        if (vida[indicer - 1, indicec + 1] == 1 || vida[indicer - 1, indicec + 1] == 2)
+                        {
+                            vecino[i, j]++;
+                        }
+                        //5
+                        if (vida[indicer, indicec + 1] == 1 || vida[indicer, indicec + 1] == 2)
+                        {
+                            vecino[i, j]++;
+                        }
+                        //7
+                        if (vida[indicer + 1, indicec] == 1 || vida[indicer + 1, indicec] == 2)
+                        {
+                            vecino[i, j]++;
+                        }
+                        //8
+                        if (vida[indicer + 1, indicec + 1] == 1 || vida[indicer + 1, indicec + 1] == 2)
+                        {
+                            vecino[i, j]++;
+                        }
+                    }
+
+                    if(j==15 && i!=0 && i!=15)
+                    {
+                        //1, 2, 4, 6, 7
+                        //1
+                        if (vida[indicer - 1, indicec - 1] == 1 || vida[indicer - 1, indicec - 1] == 2)
+                        {
+                            vecino[i, j]++;
+                        }
+                        //2
+                        if (vida[indicer - 1, indicec] == 1 || vida[indicer - 1, indicec] == 2)
+                        {
+                            vecino[i, j]++;
+                        }
+                        //4
+                        if (vida[indicer, indicec - 1] == 1 || vida[indicer, indicec - 1] == 2)
+                        {
+                            vecino[i, j]++;
+                        }
+                        //6
+                        if (vida[indicer + 1, indicec - 1] == 1 || vida[indicer + 1, indicec - 1] == 2)
+                        {
+                            vecino[i, j]++;
+                        }
+                        //7
+                        if (vida[indicer + 1, indicec] == 1 || vida[indicer + 1, indicec] == 2)
+                        {
+                            vecino[i, j]++;
+                        }
+                    }
+
+                    if (i != 0 && i != 15 && j != 0 && j != 15)
+                    {
+                        //todas
+                        //1
+                        if (vida[indicer - 1, indicec - 1] == 1 || vida[indicer - 1, indicec - 1] == 2)
+                        {
+                            vecino[i, j]++;
+                        }
+                        //2
+                        if (vida[indicer - 1, indicec] == 1 || vida[indicer - 1, indicec] == 2)
+                        {
+                            vecino[i, j]++;
+                        }
+                        //3
+                        if (vida[indicer - 1, indicec + 1] == 1 || vida[indicer - 1, indicec + 1] == 2)
+                        {
+                            vecino[i, j]++;
+                        }
+                        //4
+                        if (vida[indicer, indicec - 1] == 1 || vida[indicer, indicec - 1] == 2)
+                        {
+                            vecino[i, j]++;
+                        }
+                        //5
+                        if (vida[indicer, indicec + 1] == 1 || vida[indicer, indicec + 1] == 2)
+                        {
+                            vecino[i, j]++;
+                        }
+                        //6
+                        if (vida[indicer + 1, indicec - 1] == 1 || vida[indicer + 1, indicec - 1] == 2)
+                        {
+                            vecino[i, j]++;
+                        }
+                        //7
+                        if (vida[indicer + 1, indicec] == 1 || vida[indicer + 1, indicec] == 2)
+                        {
+                            vecino[i, j]++;
+                        }
+                        //8
+                        if (vida[indicer + 1, indicec + 1] == 1 || vida[indicer + 1, indicec + 1] == 2)
+                        {
+                            vecino[i, j]++;
+                        }
+                    }
+                }//for j
+            }//For i
+
+        }
+
+        public void Evaluar_estado()
+        {
+            string index;
+            int i, j;
+            for (i = 0; i <= 15; i++)
+            {
+                for (j = 0; j <= 15; j++)
+                {
+                    index = matrix[i, j].BackColor.ToString();
+                    switch( index )
+                    {
+                        case "Color [White]":
+                            vida[i, j] = 0;
+                            break;
+
+                        case "Color [Blue]":
+                            vida[i, j] = 1;
+                            break;
+
+                        case "Color [Green]":
+                            vida[i, j] = 2;
+                            break;
+                        default:
+                            vida[i, j] = 0;
+                            break;
+                    }
+
+                }
+            }
+        }
+
+        //Imagenes
+        public void Nueva_generacion()
+        {
+            int i, j;
+            int listo = 0;
+            for (i = 0; i <= 15; i++)
+            {
+                for (j = 0; j <= 15; j++)
+                {
+
+                    //Regla 1
+                    if (vida[i, j] == 0 && vecino[i, j] == 3 && listo == 0)
+                    {
+                        vida[i, j] = 1;
+                        matrix[i, j].BackColor = Color.Blue;
+                        matrix[i, j].Image = Image.FromFile("vivo.png");
+                        listo = 1;
+                    }
+
+                    //Regla 2
+                    //Si esta feliz permanece vivo a la siguiente
+                    if (vida[i, j] == 1 && (vecino[i, j] == 2 || vecino[i, j] == 3) && listo == 0)
+                    {
+                        vida[i, j] = 2;
+                        matrix[i, j].BackColor = Color.Green;
+                        matrix[i, j].Image = Image.FromFile("feliz.png");
+                        listo = 1;
+                    }
+                    if (vida[i, j] == 2 && listo == 0)
+                    {
+                        vida[i, j] = 1;
+                        matrix[i, j].BackColor = Color.Blue;
+                        matrix[i, j].Image = Image.FromFile("vivo.png");
+                        listo = 1;
+                    }
+                    //Regla 3
+                    if (vida[i, j] == 1 && vecino[i, j] < 2 && listo == 0)
+                    {
+                        vida[i, j] = 0;
+                        matrix[i, j].BackColor = Color.White;
+                        matrix[i, j].Image = Image.FromFile("muerto.png");
+                        listo = 1;
+                    }
+
+                    //Regla 4
+                    if (vida[i, j] == 1 && vecino[i, j] > 3 && listo == 0)
+                    {
+                        vida[i, j] = 0;
+                        matrix[i, j].BackColor = Color.White;
+                        matrix[i, j].Image = Image.FromFile("muerto.png");
+                        listo = 1;
+                    }
+                    listo = 0;
+                }
+            }
+        }
+
+        private void button_ngen_Click(object sender, EventArgs e)
+        {
+            Evaluar_estado();
+            Evaluar_vecinos();
+            Nueva_generacion();
+        }
+
+        private void buttonreinicio_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            Vida_inicio();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Evaluar_estado();
+            Evaluar_vecinos();
+            Nueva_generacion();
+        }
+
+        private void ButtonSucesivas_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+        }
+
+        private void salir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void buttonpase_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+        }
+    }
+}
